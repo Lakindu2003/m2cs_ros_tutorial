96a574e3123fb04533e9b278b82db5e254bcada8
lakindu <lakindujay@gmail.com>
2023-01-13 17:16:18 +0800

    Add assignment 1

--- turtle_path/src/path_manager.py
+++ turtle_path/src/path_manager.py
@@ -3,31 +3,47 @@ import rospy
 from math import pi, fmod, sin, cos, sqrt
 from geometry_msgs.msg import Twist
 # hint: some imports are missing
+from turtlesim.msg import Pose
+from turtle_path.srv import *
 
 cur_pos = Pose()
 
 def cb_pose(data): # get the current position from subscribing the turtle position
     global cur_pos
     cur_pos = data
-
+    
 def cb_walk(req):
     if (req.distance < 0):
         return False
-
+    
     # hint: calculate the projected (x, y) after walking the distance,
     # and return false if it is outside the boundary
-
+    x = ( cos(cur_pos.theta) * req.distance) + cur_pos.x
+    y = ( sin(cur_pos.theta) * req.distance) + cur_pos.y
+    if x > 11 or x < 0 or y < 0 or y > 11:
+        return False
+        
     rate = rospy.Rate(100) # 100Hz control loop
+    rem_dist = sqrt((x-cur_pos.x)**2 + (y-cur_pos.y)**2)
+    vel = Twist()
+    t = 100
+    vel.linear.x = x/t
+    vel.linear.y = y/t
 
-    while ('''goal not reached'''): # control loop
+    while (rem_dist <= 0.05): # control loop
         
         # in each iteration of the control loop, publish a velocity
 
         # hint: you need to use the formula for distance between two points
+        #rem_dist = sqrt((x-cur_pos.x)**2 + (y-cur_pos.y)**2)
+        rem_dist = sqrt((x-cur_pos.x)**2 + (y-cur_pos.y)**2)
+        pub.publish(vel)
         
         rate.sleep()
+        
     
-    vel = Twist() # publish a velocity 0 at the end, to ensure the turtle really stops
+    # publish a velocity 0 at the end, to ensure the turtle really stops
+    vel.linear.x = 0.0; vel.linear.y = 0.0
     pub.publish(vel)
 
     return True
@@ -35,18 +51,23 @@ def cb_walk(req):
 def cb_orientation(req):
 
     rate = rospy.Rate(100) # 100Hz control loop
+    dist = fmod(req.orientation - cur_pos.theta + pi + 2 * pi, 2 * pi) - pi
+    vel = Twist() 
+    vel.angular.z = dist/100
     
-    while ('''goal not reached'''): # control loop
+    while (dist <= 0.05): # control loop
         
         # in each iteration of the control loop, publish a velocity
 
         # hint: signed smallest distance between two angles: 
         # see https://stackoverflow.com/questions/1878907/the-smallest-difference-between-2-angles
-        #     dist = fmod(req.orientation - cur_pos.theta + pi + 2 * pi, 2 * pi) - pi
-        
+        dist = fmod(req.orientation - cur_pos.theta + pi + 2 * pi, 2 * pi) - pi
+        pub.publish(vel)
+
         rate.sleep()
     
-    vel = Twist() # publish a velocity 0 at the end, to ensure the turtle really stops
+    # publish a velocity 0 at the end, to ensure the turtle really stops
+    vel.angular.z = 0.0
     pub.publish(vel)
 
     return True
@@ -54,11 +75,12 @@ def cb_orientation(req):
 if __name__ == '__main__':
     rospy.init_node('path_manager')
     
-    pub = # publisher of the turtle velocity
-    sub = # subscriber of the turtle position, callback to cb_pose
+    pub = rospy.Publisher("velocity", Twist, queue_size = 1) # publisher of the turtle velocity
+    sub = rospy.Subscriber("position", Pose, cb_pose) # subscriber of the turtle position, callback to cb_pose
     
     ## init each service server here:
-    # rospy.Service( ... )		# callback to cb_orientation
-    # rospy.Service( ... )		# callback to cb_walk
+    srv_ori = rospy.Service("orientation", SetOrientation, cb_orientation)		# callback to cb_orientation
+    srv_walk_dis = rospy.Service("Distance", WalkDistance, cb_walk)		# callback to cb_walk
     
     rospy.spin()
+    
--- turtle_ps4/src/ps4_controller.py
+++ turtle_ps4/src/ps4_controller.py
@@ -3,29 +3,81 @@ import rospy
 from geometry_msgs.msg import Twist
 from turtlesim.srv import SetPen
 # hint: some imports are missing
+from m2_ps4.msg import Ps4Data
+from std_srvs.srv import Empty
 
 old_data = Ps4Data()
 
 def callback(data):
     global old_data
-    
+    k = 1
     # you should publish the velocity here!
     
     # hint: to detect a button being pressed, you can use the following pseudocode:
     # 
     # if ((data.button is pressed) and (old_data.button not pressed)),
     # then do something...
+    if data.dpad_y > 0 and  old_data.dpad_y <= 0:
+        k *= 2
+    if data.dpad_y < 0 and  old_data.dpad_y >= 0:
+        k /= 2
+   # if (data.hat_ly == True and  old_data.hat_ly == False) or (data.hat_rx == True and  old_data.hat_rx == False):
+   #     data.hat_ly *= k; data.hat_rx *= k
+   #     pub.publish(data)
+   #     old_data = data
     
-    old_data = data
+    if (data.hat_ly == True and  old_data.hat_ly == False):
+        vel = Twist()
+        if data.hat_ly > 0: vel.linear.y = k
+        else: vel.linear.y = -k
+        pub.publish(vel)
+        old_data = data
+    if (data.hat_rx == True and  old_data.hat_rx == False):
+        vel = Twist()
+        if data.hat_rx > 0: vel.linear.x = k
+        else: vel.linear.x = -k
+        pub.publish(vel)
+        old_data = data
+
+
+    #clear
+    if data.ps == True and old_data.ps == False:
+        srv_clr_bg(Empty())
+        old_data = data
+
+    #setpen
+    if data.Triangle == True and old_data.triangle == False:
+        req = SetPen()
+        req.g = 255; req.b = 0; req.r = 0
+        srv_col(req)
+        old_data = data
+    if data.circle == True and old_data.circle == False:
+        req = SetPen()
+        req.g = 0; req.b = 0; req.r = 255
+        srv_col(req)
+        old_data = data
+    if data.cross == True and old_data.cross == False:
+        req = SetPen()
+        req.g = 0; req.b = 255; req.r = 0
+        srv_col(req)
+        old_data = data
+    if data.square == True and old_data.square == False:
+        req = SetPen()
+        req.g = 0; req.b = 255; req.r = 255
+        srv_col(req)
+        old_data = data
+        
+    
+
 
 if __name__ == '__main__':
     rospy.init_node('ps4_controller')
     
-    pub = # publisher object goes here... hint: the topic type is Twist
-    sub = # subscriber object goes here
+    pub = rospy.Publisher("cmd_vel", Twist, queue_size = 1) # publisher object goes here... hint: the topic type is Twist
+    sub = rospy.Subscriber("ps4_data", Ps4Data, callback) # subscriber object goes here
     
     # one service object is needed for each service called!
-    srv_col = # service client object goes here... hint: the srv type is SetPen
+    srv_col = rospy.ServiceProxy("set_pen", SetPen) # service client object goes here... hint: the srv type is SetPen
     # fill in the other service client object...
-    
+    srv_clr_bg = rospy.ServiceProxy("clear", Empty)
     rospy.spin()
